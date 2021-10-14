a2717cc4b3bb2db3377702e7d4bb000bfe2861e6
zxeenu <zeenu.lastorder@gmail.com>
2021-10-14 23:51:36 +0500

    redux tested and implemented

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import { useDispatch, useSelector } from 'react-redux';
 import CardPokemon from './components/CardPokemon';
+import { decremented, incrementByAmount, incremented } from './redux/counter';
 import useFetch from './utilities/useFetch';
 
 function App() {
 
+	const { count } = useSelector(state => state.counter);
+	const dispatch = useDispatch();
+
 	const { data, error, isPending } = useFetch('https://pokeapi.co/api/v2/pokemon/');
 
-	console.log(data);
+	// console.log(data);
 
-	// const id = data["id"];
-	// const name = data["name"];
 	const types = ["green", "yello"];
 	const picture = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png";
 
@@ -20,6 +23,11 @@ function App() {
 		<div>{!isPending && data.results.map((data, index) => (
 			<div key={index}>{data.name} {data.url}</div>
 		))}</div>
+		<br />
+		<h1>count is {count}</h1>
+		<button onClick={() => dispatch(incremented()) }>Increment</button>
+		<button onClick={() => dispatch(decremented()) }>decremented</button>
+		<button onClick={() => dispatch(incrementByAmount(10)) }>by amouont</button>
 	</div>
 	);
 }
