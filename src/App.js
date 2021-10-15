5ad5b27e12b856015369d4742430efeffc57b9fb
zxeenu <zeenu.lastorder@gmail.com>
2021-10-15 21:12:26 +0500

    fetch implemented

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
+import { useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import CardPokemon from './components/CardPokemon';
-import { PING, PONG } from './redux/pokeFetch';
-import useFetch from './utilities/useFetch';
+import { BONG, loadPokemon, PING, PONG } from './redux/pokeFetch';
+// import useFetch from './utilities/useFetch';
 
 function App() {
 
-	const { isPinging } = useSelector(state => state.pokeFetch);
+	const { isPinging, pokemonData, status } = useSelector(state => state.pokeFetch);
 	const dispatch = useDispatch();
-
-	const { data, error, isPending } = useFetch('https://pokeapi.co/api/v2/pokemon/');
-
-	// console.log(data);
 
 	const types = ["green", "yello"];
 	const picture = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png";
+
+
+	useEffect(() => {
+		dispatch(loadPokemon());
+	}, [])
 
 	return (
 	<div className="App">
 		<header className="App-header">
 		</header>
 		<CardPokemon id="1" name="Bulbusor" types={types} picture={picture}/>
-		<div>{!isPending && data.results.map((data, index) => (
+		{/* <div>{!isPending && data.results.map((data, index) => (
 			<div key={index}>{data.name} {data.url}</div>
-		))}</div>
+		))}</div> */}
 		<br />
 		<h1>count is {isPinging}</h1>
+		<h1>{status}</h1>
 		<button onClick={() => dispatch(PING()) }>PING</button>
 		<button onClick={() => dispatch(PONG()) }>PONG</button>
-		{/* <button onClick={() => dispatch(incrementByAmount(10)) }>by amouont</button> */}
+		<button onClick={() => dispatch(BONG(10)) }>BONG</button>
+		<button onClick={() => dispatch(loadPokemon()) }>Get Pokemon</button>
+		<div>{pokemonData && pokemonData.map((user, i) => <div key={i}>{user.name} - {user.url}</div>)}</div>
 	</div>
 	);
 }
--- src/redux/pokeFetch.js
+++ src/redux/pokeFetch.js
@@ -1,12 +1,43 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { combineEpics, ofType } from 'redux-observable';
 import { delay, mapTo } from 'rxjs/operators';
+
+// Thunk
+const loadPokemon = createAsyncThunk(
+  "pokemon/loadPokemon",
+  async (userId, thunkAPI) => {
+
+    const state = thunkAPI.getState();
+    const res = await fetch(state.pokeFetch.currentUrl);
+    const pokemonUrlList = await res.json();
+
+    const pokemonDataList = [];
+
+    for (let i = 0; i < pokemonUrlList.results.length; i++) {
+      const pokemonResponse = await fetch(pokemonUrlList.results[i].url);
+      const pokemonData = await pokemonResponse.json();
+      pokemonDataList.push(pokemonData);
+    }
+
+    const data = {
+      dataList: pokemonDataList,
+      nextUrl: pokemonUrlList.next
+    }
+
+    return data;
+  }
+);
+
 
 // Redux state mangement
 const pokeFetch = createSlice({
   name: 'pokeFetch',
   initialState: {
-    isPinging: 'PONG'
+    currentUrl: 'https://pokeapi.co/api/v2/pokemon/',
+    isPinging: 'PONG',
+    status: '',
+    pokemonData: [],
+    pokemonDataTemp: [],
   },
   reducers: {
     PING: state => {
@@ -14,31 +45,56 @@ const pokeFetch = createSlice({
     },
     PONG: state => {
       state.isPinging = 'PING';
+    },
+    BONG: (state, action) => {
+      state.isPinging = `${action.payload}`;
     }
+  },
+  extraReducers: {
+    [loadPokemon.pending]: (state, action) => {
+      state.status = "loading";
+    },
+    [loadPokemon.fulfilled]: (state, action) => {
+      state.status = "sucess";
+      state.currentUrl = action.payload.nextUrl;
+      state.pokemonDataTemp = [...action.payload.dataList];
+
+      if (state.pokemonDataTemp.length != 0) {
+        state.pokemonData = [...state.pokemonData, ...state.pokemonDataTemp];
+      } else {
+        state.pokemonData = [...state.pokemonDataTemp];
+      }
+      // console.log(state.pokemonData);
+    },
+    [loadPokemon.rejected]: (state, action) => {
+      state.status = "failed";
+    },
   }
 });
 
 
 // A single Epic for the this duck
-const pingToPong = action$ => action$.pipe(
+const pingToPong = (action$ ) => action$.pipe(
   ofType(PING),
   delay(5000), // Asynchronously wait 1000ms then continue
   mapTo({ type: PONG })
 );
 
-const pongToPing = action$ => action$.pipe(
+const pongToPing = (action$) => action$.pipe(
   ofType(PONG),
   delay(5000), // Asynchronously wait 1000ms then continue
   mapTo({ type: PING })
 );
 
+
 // the epic (Redux Observable middleware.)
 // actions pass from the middleware, to the reducers.
 const pokeFetchEpic = combineEpics(
   pingToPong,
-  pongToPing
+  pongToPing,
 );
 
-export const { PING, PONG } = pokeFetch.actions;
+export const { PING, PONG, BONG } = pokeFetch.actions;
 export { pokeFetchEpic };
+export { loadPokemon };
 export default pokeFetch.reducer;
